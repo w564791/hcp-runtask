7a2e105fca775e0e051e21131e7efe307d1cd8e9
w564791 <how_bjl@live.cn>
2025-01-01 03:49:06 +0000

    update

--- sg.tf
+++ sg.tf
@@ -37,6 +37,9 @@ resource "aws_vpc_security_group_ingress_rule" "none-tag" {
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
+  tags = {
+     Name = "example"
+  }
 }
 resource "aws_vpc_security_group_egress_rule" "none-to-all" {
   security_group_id = aws_security_group.example.id
