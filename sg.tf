d3ccbb033b3f9a7b9aa27d221e95b2e9bd4207ed
w564791 <how_bjl@live.cn>
2025-01-01 03:03:50 +0000

    update

--- sg.tf
+++ sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "example" {
 resource "aws_vpc_security_group_ingress_rule" "example" {
   security_group_id = aws_security_group.example.id
 
-  cidr_ipv4   = "8.8.8.8/32"
+  cidr_ipv4   = "8.8.8.9/32"
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
@@ -21,7 +21,7 @@ resource "aws_vpc_security_group_ingress_rule" "example" {
 resource "aws_vpc_security_group_ingress_rule" "prefix" {
   security_group_id = aws_security_group.example.id
 
-  prefix_list_id = "pl-xxxx008"
+  prefix_list_id = "pl-xxxx"
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
@@ -32,11 +32,3 @@ resource "aws_vpc_security_group_ingress_rule" "prefix" {
 
 
 
-resource "aws_security_group_rule" "rejected" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "tcp"
-  cidr_blocks       = ["1.1.1.1/32"]
-  security_group_id = aws_security_group.example.id
-}
