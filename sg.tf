7421a59ee81463f6ed7fcb469422d3b5880cc39e
w564791 <how_bjl@live.cn>
2025-01-01 03:52:13 +0000

    update

--- sg.tf
+++ sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "example" {
 resource "aws_vpc_security_group_ingress_rule" "example" {
   security_group_id = aws_security_group.example.id
 
-  cidr_ipv4   = "8.8.8.9/32"
+  cidr_ipv4   = "8.8.8.8/32"
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
