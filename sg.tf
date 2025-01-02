b9f375d880b11bae19b545277a7f6de00838caaf
w564791 <how_bjl@live.cn>
2025-01-02 10:52:25 +0000

    update

--- sg.tf
+++ sg.tf
@@ -80,3 +80,12 @@ resource "aws_vpc_security_group_ingress_rule" "prefix-xxx" {
      Name = "example"
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "no-tags" {
+  security_group_id = aws_security_group.example.id
+
+  prefix_list_id = "pl-xxx"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
