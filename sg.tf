00aba6d37be7eb77eeaf896925dad33d51fc8928
w564791 <how_bjl@live.cn>
2025-01-03 15:31:47 +0000

    remove

--- sg.tf
+++ sg.tf
@@ -63,7 +63,7 @@ resource "aws_vpc_security_group_ingress_rule" "none-tag" {
 resource "aws_vpc_security_group_egress_rule" "none-to-all" {
   security_group_id = aws_security_group.example.id
 
-  prefix_list_id = "pl-abc"
+  prefix_list_id = "pl-xxx"
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
@@ -116,4 +116,7 @@ resource "aws_vpc_security_group_ingress_rule" "no-tags-name" {
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
+tags = {
+     Name = "example"
+  }
 }
