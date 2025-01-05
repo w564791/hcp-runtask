cadb069f949822753384e5323aec88edafc6c79b
w564791 <how_bjl@live.cn>
2025-01-05 07:15:56 +0000

    update

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "web" {
   ami           = "ami-acd"
   instance_type = "t3.micro"
-
+  associate_public_ip_address = false
+  metadata_options {
+    http_tokens = "required"
+    http_endpoint = "enabled"
+  }
   tags = {
     Name = "HelloWorld"
   }
