db972bf9ea6cd8145cfafcfbcab7595e852e5684
w564791 <how_bjl@live.cn>
2025-01-05 07:30:01 +0000

    add volume tag component

---
+++ ec2-se.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "se" {
+  ami           = "ami-acd"
+  instance_type = "t3.micro"
+  associate_public_ip_address = false
+  metadata_options {
+    http_tokens = "required"
+    http_endpoint = "enabled"
+  }
+  volume_tags =  {
+    Name = "example"
+    managed-by = "abc"
+    business-line = "cde"
+    component = "mid"
+    backup-enabled = "acc"
+    eee = "ffc"
+    owner = "abcd"
+  }
+  tags = {
+    Name = "example"
+    managed-by = "abc"
+    business-line = "cde"
+    component = "mid"
+    backup-enabled = "acc"
+    eee = "ffc"
+    owner = "abcd"
+    service-name = "dd"
+  }
+}
