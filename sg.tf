45c25f586d81bdf3a15cdd263b377a39b81a5dc3
w564791 <how_bjl@live.cn>
2025-01-07 07:41:05 +0000

    update

--- sg.tf
+++ sg.tf
@@ -28,7 +28,7 @@ resource "aws_vpc_security_group_ingress_rule" "example" {
   security_group_id = aws_security_group.example.id
 
   cidr_ipv4   = "8.8.8.8/32"
-  from_port   = 80
+  from_port   = 22
   ip_protocol = "tcp"
   to_port     = 80
   tags = {
