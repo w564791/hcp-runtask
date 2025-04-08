ee1bc609d1c75658214c8161c63b83578dc5125c
w564791 <how_bjl@live.cn>
2025-04-08 06:11:32 +0000

    update

--- ec2-se.tf
+++ ec2-se.tf
@@ -9,9 +9,10 @@ resource "aws_instance" "se" {
   }
   root_block_device {
     encrypted  = true
+    device_name = '/dev/sdb'
   }
   ebs_block_device {
-    
+    device_name = '/dev/sdh'
   }
   volume_tags =  {
 
