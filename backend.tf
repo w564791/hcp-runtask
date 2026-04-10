ecaee8a33e05d2d120ad0ecafd5fd24a58b0f20e
w564791 <how_bjl@live.cn>
2026-04-10 02:41:08 +0000

    remove all of aws

--- backend.tf
+++ backend.tf
@@ -16,9 +16,9 @@ terraform {
 }
 
 
-provider "aws" {
-  region = "ap-northeast-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
+#provider "aws" {
+#  region = "ap-northeast-1"
+#  access_key = var.AWS_ACCESS_KEY_ID
+#  secret_key = var.AWS_SECRET_ACCESS_KEY
+#}
 
